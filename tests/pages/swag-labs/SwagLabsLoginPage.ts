38d1ea024b02f9e7c60b098e79cb324a3cac557f
József Hajas <hayee4@gmail.com>
2025-10-16 21:34:10 +0200

    Add login with performance glitch user method and rename header

--- tests/pages/swag-labs/SwagLabsLoginPage.ts
+++ tests/pages/swag-labs/SwagLabsLoginPage.ts
@@ -1,26 +1,30 @@
-import { Page, Locator } from '@playwright/test';
+import { Locator, Page } from '@playwright/test';
+import creds from '../../resources/swag-labs/credential.json';
 
 export class SwagLabsLoginPage {
 
     readonly page: Page;
-    readonly title: Locator;
+    readonly header: Locator;
     readonly usernameInput: Locator;
     readonly passwordInput: Locator;
     readonly loginButton: Locator;
 
     constructor(page: Page) {
-        // Using mixed locator strategies
         this.page = page;
-        this.title = page.locator('.login_logo'); // Locator for the title element using class name
-        this.usernameInput = page.getByTestId('username'); // Locator for the username input field using data-test
-        this.passwordInput = page.getByLabel('Password'); // Locator for the password input field using label
-        this.loginButton = page.getByRole('button', { name: 'Login' }); // Locator for the login button using role and name
+        this.header = page.locator('.login_logo');
+        this.usernameInput = page.getByPlaceholder('Username');
+        this.passwordInput = page.locator('#password');
+        this.loginButton = page.getByRole('button', { name: 'Login' });
     }
 
     async goto() {
         await this.page.goto('https://www.saucedemo.com');
     }
 
-    
+    async loginWithPerformanceGlitchUser() {
+        await this.usernameInput.fill(creds.username);
+        await this.passwordInput.fill(creds.password);
+        await this.loginButton.click();
+    }
 }
 
