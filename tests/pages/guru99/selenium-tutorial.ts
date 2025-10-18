4e9dbc9de8c74865a973e42df3630191c3386e81
József Hajas <hayee4@gmail.com>
2025-10-18 19:02:51 +0200

    Add selenium tutorial page class

---
+++ tests/pages/guru99/selenium-tutorial.ts
@@ -0,0 +1,18 @@
+import { Page, Locator } from "@playwright/test";
+import { BasePage } from "../base-page";
+
+export class SeleniumTutorialPage extends BasePage {
+
+    readonly title: Locator;
+    readonly submitButton: Locator;
+
+    constructor(page: Page) {
+        super(page, "https://www.guru99.com/selenium-tutorial.html");
+        this.title = page.locator('h1');
+        this.submitButton = page.locator('button[type="submit"]');
+    }
+
+    async waitForSeleniumTutorialPageToLoad() {
+        await this.waitForElementVisible(this.title);
+    }
+}
