2d8adc68316cb4697f16ab758e409485de0f4600
József Hajas <hayee4@gmail.com>
2025-10-18 12:50:17 +0200

    Introduce footer page file

---
+++ tests/pages/swag-labs/footer-page.ts
@@ -0,0 +1,19 @@
+import { Locator, Page } from '@playwright/test';
+import { BasePage } from '../base-page';
+
+export class SwagLabsFooterPage extends BasePage {
+
+    readonly footer: Locator;
+    readonly footerText: Locator;
+
+    constructor(page: Page) {
+        super(page);
+        this.footer = page.locator('footer');
+        this.footerText = page.locator('.footer_copy');
+    }
+
+    async getFooterText(): Promise<string | null> {
+        await this.footer.waitFor({ state: 'visible' });
+        return this.footerText.textContent();
+    }
+}   
