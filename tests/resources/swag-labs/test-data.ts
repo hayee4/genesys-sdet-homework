34581f578c2771aa5ba7965beacc2bbfe84c8682
József Hajas <hayee4@gmail.com>
2025-10-18 11:22:22 +0200

    Introduce test data file

---
+++ tests/resources/swag-labs/test-data.ts
@@ -0,0 +1,23 @@
+const SWAG_LABS_ITEMS = {
+    BACKPACK: "Sauce Labs Backpack",
+    FLEECE_JACKET: "Sauce Labs Fleece Jacket", 
+    BIKE_LIGHT: "Sauce Labs Bike Light",
+    BOLT_TSHIRT: "Sauce Labs Bolt T-Shirt",
+    ONESIE: "Sauce Labs Onesie",
+    RED_TSHIRT: "Test.allTheThings() T-Shirt (Red)"
+}
+
+export const CUSTOMER_INFO = {
+    VALID_CUSTOMER: {
+        firstName: "John",
+        lastName: "Doe", 
+        zipCode: "12345"
+    }
+}
+
+export const TEST_SCENARIOS = {
+    TWO_ITEM_PURCHASE: {
+        items: [SWAG_LABS_ITEMS.BACKPACK, SWAG_LABS_ITEMS.FLEECE_JACKET],
+        description: "Purchase Backpack and Fleece Jacket"
+    }
+}
