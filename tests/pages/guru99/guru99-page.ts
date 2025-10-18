112ccae7375a1cd8531a2f0b004c90bae0c524b6
József Hajas <hayee4@gmail.com>
2025-10-18 19:03:06 +0200

    Extend guru99 file

--- tests/pages/guru99/guru99-page.ts
+++ tests/pages/guru99/guru99-page.ts
@@ -1,18 +1,28 @@
 import { Locator, Page } from "@playwright/test";
 import { BasePage } from "../base-page";
 
-export class Guru99Page extends BasePage {
+export class Guru99MainPage extends BasePage {
 
     readonly title: Locator;
     readonly iFrame: Locator;
+    readonly menuContainer: Locator;
+    readonly testingLink: Locator;
+    readonly seleniumLink: Locator;
 
     constructor(page: Page) {
         super(page, "https://demo.guru99.com/test/guru99home/");
         this.title = page.locator("h2:has-text('THIS IS A DEMO PAGE FOR TESTING')");
         this.iFrame = page.locator('iframe#a077aa5e');
+        this.menuContainer = page.locator('.rt-container');
+        this.testingLink = this.menuContainer.getByRole('link', { name: 'Testing' }).first();
+        this.seleniumLink = this.menuContainer.getByRole('link', { name: 'Selenium' }).first();
     }
 
     async waitForGuru99PageToLoad() {
         await this.waitForElementVisible(this.title);
+    }
+
+    async waitForSubMenuToLoad() {
+        await this.waitForElementVisible(this.seleniumLink);
     }
 }
