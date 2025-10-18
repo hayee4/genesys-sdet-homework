3d8f72f69269a2b6741798cc8b7518d24770b988
József Hajas <hayee4@gmail.com>
2025-10-18 09:35:14 +0200

    Introduce Base Page class

---
+++ tests/pages/base-page.ts
@@ -0,0 +1,22 @@
+import { Page, Locator } from "@playwright/test";
+
+export class BasePage {
+
+    readonly page: Page;
+    readonly url?: string;
+
+    constructor(page: Page, url?: string) {
+        this.page = page;
+        this.url = url;
+    }
+
+    async goto() {
+        if (this.url) {
+            await this.page.goto(this.url);
+        }
+    }
+
+    async waitForElementVisible(locator: Locator) {
+        await locator.waitFor({ state: 'visible' });
+    }
+}
