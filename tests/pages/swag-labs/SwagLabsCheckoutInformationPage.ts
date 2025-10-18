cb98072e53ec4c5a0196818ef52be4b5a172a3e9
Jozsef3.Hajas <jozsef3.hajas@evosoft.com>
2025-10-18 08:36:21 +0200

    Add Swag Labs Checkout Information related page file

---
+++ tests/pages/swag-labs/SwagLabsCheckoutInformationPage.ts
@@ -0,0 +1,28 @@
+import {Page, Locator} from "@playwright/test";
+
+export class SwagLabsCheckoutInformationPage {
+
+    readonly page: Page;
+    readonly checkoutTitle: Locator;
+    readonly firstNameInput: Locator;
+    readonly lastNameInput: Locator;
+    readonly postalCodeInput: Locator;
+    readonly continueButton: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.checkoutTitle = page.locator('.title');
+        this.firstNameInput = page.getByPlaceholder('First Name');
+        this.lastNameInput = page.getByPlaceholder('Last Name');
+        this.postalCodeInput = page.getByPlaceholder('Zip/Postal Code');
+        this.continueButton = page.getByRole('button', { name: 'Continue' });
+    }
+
+    async fillCheckoutInformation(firstName: string, lastName: string, postalCode: string) {
+        await this.firstNameInput.fill(firstName);
+        await this.lastNameInput.fill(lastName);
+        await this.postalCodeInput.fill(postalCode);
+        await this.continueButton.click();
+    }
+
+}
