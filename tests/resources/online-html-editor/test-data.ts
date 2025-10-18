8e15f1fdfddd12925f1f16c56c5e623272e4ee99
József Hajas <hayee4@gmail.com>
2025-10-18 15:29:05 +0200

    Add test data file for online html editor

---
+++ tests/resources/online-html-editor/test-data.ts
@@ -0,0 +1,12 @@
+export const TEST_TEXT = {
+    BOLD: 'Automation',
+    UNDERLINED: 'Test',
+    PLAIN: 'Example',
+    SPACE: ' '
+}
+
+export const EXPECTED_HTML = {
+    BOLD_TAG: `<strong>${TEST_TEXT.BOLD}</strong>`,
+    UNDERLINED_TAG: `<u>${TEST_TEXT.UNDERLINED}</u>`,
+    PLAIN_TEXT: `${TEST_TEXT.PLAIN}`
+}
