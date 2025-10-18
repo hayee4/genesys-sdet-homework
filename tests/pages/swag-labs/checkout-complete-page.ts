bbd5c09f4c055694095a655637906c17e5056a4e
József Hajas <hayee4@gmail.com>
2025-10-18 11:22:38 +0200

    Add complete header and title locators

--- tests/pages/swag-labs/checkout-complete-page.ts
+++ tests/pages/swag-labs/checkout-complete-page.ts
@@ -4,10 +4,14 @@ import { BasePage } from "../base-page";
 export class SwagLabsCheckoutCompletePage extends BasePage {
 
     readonly title: Locator;
+    readonly completeHeader: Locator;
+    readonly completeText: Locator;
     readonly backHomeButton: Locator;
 
     constructor(page: Page) {
         super(page, "https://www.saucedemo.com/checkout-complete.html");
+        this.completeHeader = page.locator('.complete-header');
+        this.completeText = page.locator('.complete-text');
         this.title = page.locator('.title');
         this.backHomeButton = page.getByRole('button', { name: 'Back Home' });
     }
