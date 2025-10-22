d4df6ab100a6ce28a6b57555c99d5c09c55ff1bf
József Hajas <hayee4@gmail.com>
2025-10-22 08:01:00 +0200

    Refactor to use private access modifiers for properties

--- tests/pages/swag-labs/checkout-complete-page.ts
+++ tests/pages/swag-labs/checkout-complete-page.ts
@@ -2,20 +2,42 @@ import { Locator, Page } from '@playwright/test';
 import { BasePage } from '../base-page';
 
 export class SwagLabsCheckoutCompletePage extends BasePage {
-    readonly title: Locator;
-    readonly completeHeader: Locator;
-    readonly completeText: Locator;
-    readonly backHomeButton: Locator;
+    private readonly title: Locator;
+    private readonly completeHeader: Locator;
+    private readonly completeText: Locator;
+    private readonly backHomeButton: Locator;
 
     constructor(page: Page) {
         super(page, 'https://www.saucedemo.com/checkout-complete.html');
-        this.completeHeader = page.locator('.complete-header');
-        this.completeText = page.locator('.complete-text');
-        this.title = page.locator('.title');
-        this.backHomeButton = page.getByRole('button', { name: 'Back Home' });
+        this.completeHeader = this.page.locator('.complete-header');
+        this.completeText = this.page.locator('.complete-text');
+        this.title = this.page.locator('.title');
+        this.backHomeButton = this.page.getByRole('button', { name: 'Back Home' });
     }
 
-    async waitForCheckoutCompletePageToLoad() {
+    public async returnToProductsPage() {
+        await this.backHomeButton.click();
+    }
+
+    public async waitForCheckoutCompletePageToLoad() {
         await this.waitForElementVisible(this.title);
+    }
+
+    public async waitForCompleteHeaderToBeVisible() {
+        await this.waitForElementVisible(this.completeHeader);
+    }
+
+    public async waitForCompleteTextToBeVisible() {
+        await this.waitForElementVisible(this.completeText);
+    }
+
+    public async getCompleteHeader() {
+        await this.waitForElementVisible(this.completeHeader);
+        return await this.completeHeader.textContent();
+    }
+
+    public async getCompleteText() {
+        await this.waitForElementVisible(this.completeText);
+        return await this.completeText.textContent();
     }
 }
