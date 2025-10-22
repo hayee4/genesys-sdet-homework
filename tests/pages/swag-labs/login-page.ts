c02d87d1b267d4320d8187a2d8d6f9d2b7f21fae
József Hajas <hayee4@gmail.com>
2025-10-22 07:17:46 +0200

    Refactor SwagLabsLoginPage to use access modifiers

--- tests/pages/swag-labs/login-page.ts
+++ tests/pages/swag-labs/login-page.ts
@@ -2,37 +2,41 @@ import { Locator, Page } from '@playwright/test';
 import { BasePage } from '../base-page';
 
 export class SwagLabsLoginPage extends BasePage {
-    readonly headerText: Locator;
-    readonly usernameInput?: Locator;
-    readonly passwordInput?: Locator;
-    readonly loginButton: Locator;
-    readonly errorMessage: Locator;
+    private readonly headerText: Locator;
+    private readonly usernameInput?: Locator;
+    private readonly passwordInput?: Locator;
+    private readonly loginButton: Locator;
+    private readonly errorMessage: Locator;
 
     constructor(page: Page) {
         super(page, 'https://www.saucedemo.com/');
-        this.headerText = page.locator('.login_logo');
-        this.usernameInput = page.getByPlaceholder('Username');
-        this.passwordInput = page.getByPlaceholder('Password');
-        this.loginButton = page.getByRole('button', { name: 'Login' });
-        this.errorMessage = page.locator('[data-test="error"]');
+        this.headerText = this.page.locator('.login_logo');
+        this.usernameInput = this.page.getByPlaceholder('Username');
+        this.passwordInput = this.page.getByPlaceholder('Password');
+        this.loginButton = this.page.getByRole('button', { name: 'Login' });
+        this.errorMessage = this.page.locator('[data-test="error"]');
     }
 
-    async login(username?: string, password?: string) {
+    public async login(username?: string, password?: string) {
         if (username != undefined) {
             await this.usernameInput?.fill(username);
         }
-
         if (password != undefined) {
             await this.passwordInput?.fill(password);
         }
         await this.loginButton.click();
     }
 
-    async waitForErrorMessageToBeVisible() {
+    public async getErrorMessageText() {
+        await this.waitForElementVisible(this.errorMessage);
+        return await this.errorMessage.textContent();
+    }
+
+    public async waitForErrorMessageToBeVisible() {
         await this.waitForElementVisible(this.errorMessage);
     }
 
-    async waitForLoginPageToLoad() {
+    public async waitForLoginPageToLoad() {
         await this.waitForElementVisible(this.headerText);
     }
 }
