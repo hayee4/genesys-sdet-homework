7489d8574834279d29feac82c7568d03ceba2a2c
József Hajas <hayee4@gmail.com>
2025-10-22 07:20:45 +0200

    Refactor SwagLabsLoginPage to use access modifiers

--- tests/pages/swag-labs/products-page.ts
+++ tests/pages/swag-labs/products-page.ts
@@ -2,22 +2,23 @@ import { Locator, Page } from '@playwright/test';
 import { BasePage } from '../base-page';
 
 export class SwagLabsProductsPage extends BasePage {
-    readonly title: Locator;
+    private readonly title: Locator;
 
-    itemContainerByName = (itemName: string) => this.page.locator('.inventory_item').filter({ hasText: itemName });
+    private itemContainerByName = (itemName: string) =>
+        this.page.locator('.inventory_item').filter({ hasText: itemName });
 
     constructor(page: Page) {
         super(page, 'https://www.saucedemo.com/inventory.html');
-        this.title = page.locator('.title');
+        this.title = this.page.locator('.title');
     }
 
-    async addItemToCartByName(itemName: string) {
+    public async addItemToCartByName(itemName: string) {
         const itemContainer = this.itemContainerByName(itemName);
         const addToCartButton = itemContainer.getByRole('button', { name: 'Add to cart' });
         await addToCartButton.click();
     }
 
-    async waitForProductsPageToLoad() {
+    public async waitForProductsPageToLoad() {
         await this.waitForElementVisible(this.title);
     }
 }
