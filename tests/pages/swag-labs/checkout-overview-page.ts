94b18b3e6141ca8098a9dffa1cc55166e43b0913
József Hajas <hayee4@gmail.com>
2025-10-22 07:49:16 +0200

    Refactor to use private access modifiers for properties

--- tests/pages/swag-labs/checkout-overview-page.ts
+++ tests/pages/swag-labs/checkout-overview-page.ts
@@ -2,20 +2,24 @@ import { Locator, Page } from '@playwright/test';
 import { BasePage } from '../base-page';
 
 export class SwagLabsCheckoutOverviewPage extends BasePage {
-    readonly title: Locator;
-    readonly finishButton: Locator;
+    private readonly title: Locator;
+    private readonly finishButton: Locator;
 
     constructor(page: Page) {
         super(page, 'https://www.saucedemo.com/checkout-step-two.html');
-        this.title = page.locator('.title');
-        this.finishButton = page.getByRole('button', { name: 'Finish' });
+        this.title = this.page.locator('.title');
+        this.finishButton = this.page.getByRole('button', { name: 'Finish' });
     }
 
-    async getItemByName(itemName: string) {
+    public async getItemByName(itemName: string) {
         return this.page.locator(`.cart_item:has-text("${itemName}")`);
     }
 
-    async waitForCheckoutOverviewPageToLoad() {
+    public async clickFinishButton() {
+        await this.finishButton.click();
+    }
+
+    public async waitForCheckoutOverviewPageToLoad() {
         await this.waitForElementVisible(this.title);
     }
 }
