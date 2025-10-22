afe0ac233b4bd592d2dded5bb85a705602351a17
József Hajas <hayee4@gmail.com>
2025-10-22 07:50:45 +0200

    Refactor to use private access modifiers for properties

--- tests/pages/swag-labs/checkout-information-page.ts
+++ tests/pages/swag-labs/checkout-information-page.ts
@@ -2,29 +2,29 @@ import { Locator, Page } from '@playwright/test';
 import { BasePage } from '../base-page';
 
 export class SwagLabsCheckoutInformationPage extends BasePage {
-    readonly title: Locator;
-    readonly firstNameInput: Locator;
-    readonly lastNameInput: Locator;
-    readonly postalCodeInput: Locator;
-    readonly continueButton: Locator;
+    private readonly title: Locator;
+    private readonly firstNameInput: Locator;
+    private readonly lastNameInput: Locator;
+    private readonly postalCodeInput: Locator;
+    private readonly continueButton: Locator;
 
     constructor(page: Page) {
         super(page, 'https://www.saucedemo.com/checkout-step-one.html');
-        this.title = page.locator('.title');
-        this.firstNameInput = page.getByPlaceholder('First Name');
-        this.lastNameInput = page.getByPlaceholder('Last Name');
-        this.postalCodeInput = page.getByPlaceholder('Zip/Postal Code');
-        this.continueButton = page.getByRole('button', { name: 'Continue' });
+        this.title = this.page.locator('.title');
+        this.firstNameInput = this.page.getByPlaceholder('First Name');
+        this.lastNameInput = this.page.getByPlaceholder('Last Name');
+        this.postalCodeInput = this.page.getByPlaceholder('Zip/Postal Code');
+        this.continueButton = this.page.getByRole('button', { name: 'Continue' });
     }
 
-    async fillCheckoutInformation(firstName: string, lastName: string, postalCode: string) {
+    public async fillCheckoutInformation(firstName: string, lastName: string, postalCode: string) {
         await this.firstNameInput.fill(firstName);
         await this.lastNameInput.fill(lastName);
         await this.postalCodeInput.fill(postalCode);
         await this.continueButton.click();
     }
 
-    async waitForCheckoutInformationPageToLoad() {
+    public async waitForCheckoutInformationPageToLoad() {
         await this.waitForElementVisible(this.title);
     }
 }
