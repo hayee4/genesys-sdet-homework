5f85368f2b2ad69c0beebd0d6bfa1941ed09592b
József Hajas <hayee4@gmail.com>
2025-10-22 06:56:52 +0200

    Refactor BasePage class to use protected access modifiers and update method visibility

--- tests/pages/base-page.ts
+++ tests/pages/base-page.ts
@@ -1,21 +1,25 @@
 import { Locator, Page } from '@playwright/test';
 
-export class BasePage {
-    readonly page: Page;
-    readonly url?: string;
+export abstract class BasePage {
+    protected readonly page: Page;
+    protected readonly url?: string;
 
     constructor(page: Page, url?: string) {
         this.page = page;
         this.url = url;
     }
 
-    async goto() {
+    public async goto() {
         if (this.url) {
             await this.page.goto(this.url, { waitUntil: 'domcontentloaded', timeout: 60000 });
         }
     }
 
-    async waitForElementVisible(locator: Locator) {
+    public getUrl() {
+        return this.url;
+    }
+
+    protected async waitForElementVisible(locator: Locator) {
         await locator.waitFor({ state: 'visible' });
     }
 }
